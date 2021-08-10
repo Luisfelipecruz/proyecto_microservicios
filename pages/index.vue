74280c9d4eecc601d5d6c0767372926007bfaf3a
Carlos <carlosdsanjuan@outlook.com>
2021-08-09 20:48:55 -0500

    Creacion del proyecto en nuxt

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style lang="scss">
+:root {
+
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 62.5%;
+}
+</style>

59067da57cfe8ac9f80bba705f58783da6e3dc26
LuisFelipeCruz <luisfelipecruzgeo@hotmail.com>
2021-08-21 17:36:50 -0500

    feat(baseProyecto): creacion de la base del proyecto

---
+++ back-end/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+mysqlconnector://root:{password}@localhost:3306/{dataBaseName}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
